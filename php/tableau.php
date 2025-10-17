e6b9c6bc66c593a79c94351c63a8ce06c71c74b5
damienrabelais <damien.boucard@ac-rennes.fr>
2025-10-17 16:48:04 +0200

    PHP : tableau 2 dimensions

--- php/tableau.php
+++ php/tableau.php
@@ -1,7 +1,17 @@
 <?php
 echo "<table border=1>";
-for($noLigne = 0; $noLigne < 100; $noLigne++) {
-    echo "<tr><td>".$noLigne."</td></tr>";
+for ($noLigne = 0; $noLigne < 10; $noLigne++) {
+    echo "<tr><td>" . $noLigne . "</td></tr>";
 }
 echo "</table>";
-?>
+
+echo "Tables de multiplication";
+echo "<table border=1>";
+for ($n = 1; $n <= 10; $n++) {
+    echo "<tr>";
+    for ($facteur = 1; $facteur <= 10; $facteur++) {
+        echo "<td>" . ($n * $facteur) . "</td>";
+    }
+    echo "</tr>";
+}
+echo "</table>";
